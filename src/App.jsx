f580490a72bf32084ad8c5c1aaef758d47bab9a8
Sayeed <sayeedulhoda@gmail.com>
2023-09-06 01:41:08 +0530

    complete the project but css is remaing and have to add local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
+import { ToastContainer } from 'react-toastify';
+import { nanoid } from 'nanoid';
+import { useState } from 'react';
+import Form from './Form.jsx';
+import Items from './Items.jsx';
+
 const App = () => {
-  return <h2>Grocery Bud - Starter</h2>;
+	const [items, setItems] = useState([]);
+
+	const addItem = (itemName) => {
+		const newItem = {
+			name: itemName,
+			completed: false,
+			id: nanoid(),
+		};
+		setItems([...items, newItem]);
+	};
+	const removeItem = (itemId) => {
+		const newItems = items.filter((item) => item.id !== itemId);
+		setItems(newItems);
+	};
+	return (
+		<section className="section-center">
+			<Form addItem={addItem} />
+			<Items
+				items={items}
+				removeItem={removeItem}
+			/>
+			<ToastContainer position="top-center" />
+		</section>
+	);
 };
 
 export default App;
