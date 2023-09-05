e94a2979df1d1a55a864e15c540f4fb08e09d2e9
Sayeed <sayeedulhoda@gmail.com>
2023-09-06 02:21:54 +0530

    just css remaning

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 import { nanoid } from 'nanoid';
 import { useState } from 'react';
 import Form from './Form.jsx';
 import Items from './Items.jsx';
 
+const setLocalStorage = (items) => {
+	localStorage.setItem('list', JSON.stringify(items));
+};
+const defaultList = JSON.parse(localStorage.getItem('list') || '[]');
 const App = () => {
-	const [items, setItems] = useState([]);
+	const [items, setItems] = useState(defaultList);
 
 	const addItem = (itemName) => {
 		const newItem = {
@@ -13,18 +17,36 @@ const App = () => {
 			completed: false,
 			id: nanoid(),
 		};
-		setItems([...items, newItem]);
+		const newItems = [...items, newItem];
+		setItems(newItems);
+		setLocalStorage(newItems);
+		toast.success('add item to the list');
 	};
 	const removeItem = (itemId) => {
 		const newItems = items.filter((item) => item.id !== itemId);
 		setItems(newItems);
+		setLocalStorage(newItems);
+		toast.success('item remove');
 	};
+	const editItem = (itemId) => {
+		const newItems = items.map((item) => {
+			if (item.id == itemId) {
+				const newItem = { ...item, completed: !item.completed };
+				return newItem;
+			}
+			return item;
+		});
+		setItems(newItems);
+		setLocalStorage(newItems);
+	};
+
 	return (
 		<section className="section-center">
 			<Form addItem={addItem} />
 			<Items
 				items={items}
 				removeItem={removeItem}
+				editItem={editItem}
 			/>
 			<ToastContainer position="top-center" />
 		</section>
--- src/SingleItem.jsx
+++ src/SingleItem.jsx
@@ -1,17 +1,16 @@
 import { useState } from 'react';
 
-const SingleItem = ({ item, removeItem }) => {
-	const [isChecked, setIsChecked] = useState(item.completed);
+const SingleItem = ({ item, removeItem, editItem }) => {
 	return (
 		<div className="single-item">
 			<input
 				type="checkbox"
-				checked={isChecked}
+				checked={item.completed}
 				onChange={() => {
-					setIsChecked(!isChecked);
+					editItem(item.id);
 				}}
 			/>
-			<p style={{ textDecoration: isChecked && 'line-through' }}>{item.name}</p>
+			<p style={{ textDecoration: item.completed && 'line-through' }}>{item.name}</p>
 			<button
 				className="btn remove-btn"
 				onClick={() => {
